c3c69d163e56424902f5ebf9056576f6d3ba930c
pelumi <oluboyepelumi2018@gmail.com>
2021-10-29 17:40:28 +0100

    SUM AND PRODUCT

---
+++ src/intro/sum_and_product.py
@@ -0,0 +1,18 @@
+def sum(x: int, y: int, z: int):
+    addition = x + y + z
+    return addition
+
+
+first_num = input("first number")
+second_num = input("second number")
+third_num = input("third number")
+
+print(sum(int(first_num), int(second_num), int(third_num)))
+
+
+def product(x: int, y: int, z: int):
+    multiply = x * y * z
+    return multiply
+
+
+print(product(int(first_num), int(second_num), int(third_num)))
